945a0999ac10ac93a9b6a3350b4b994146b474db
Sahil Tambe <sahiltambe81922@gmail.com>
2023-09-27 00:45:24 +0530

    homepage done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import Profile from '../assets/Profile.jpeg'
 import Icon from '../assets/Nv.jsx'
+import google from '../assets/google.png'
+import { ImSearch, ImMic } from 'react-icons/im'
+import { BsKeyboardFill } from 'react-icons/bs'
 
 const Home = () => {
   return (
     <>
-      <nav >
+      <nav className='flex text-sm mb-28  items-center justify-end gap-6 p-4' >
+
+        <p>Gmail</p>
+        <p>Images</p>
         <Icon />
+        <img className='w-10 rounded-full' src={Profile} alt="profile" />
       </nav>
+      <div className='flex flex-col items-center gap-5'>
+        <img className='w-36 sm:w-56' src={google} alt="google" />
+        <div className='h-11 w-72 sm:w-[32rem] text-slate-400 flex justify-between items-center p-2 border-[#E3E3E3] border-2 border-solid  rounded-full'>
+          <ImSearch />
+          <div className='flex gap-3'>
+            <BsKeyboardFill />
+            <ImMic />
+          </div>
+        </div>
+        <div className='flex gap-8'>
+          <button className='btn '>Search</button>
+          <button className='btn'>Im feeling lucky</button>
+        </div>
+      </div>
+      <footer className='bg-[#F2F2F2F2] text-slate-500 flex gap-3 flex-col p-2  w-screen absolute bottom-0'>
+        <p>India</p>
+        <div className='flex flex-col sm:flex-row justify-between items-center '>
+          <div className='flex gap-5  '>
+            <p>About</p>
+            <p>Advertising</p>
+            <p>Business</p>
+            <p>How Search works</p>
+          </div>
+          <div className='flex gap-5'>
+            <p>Privacy</p>
+            <p>Terms</p>
+            <p>Settings</p>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
